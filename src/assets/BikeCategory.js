5974c020bc8c45a5921df09b372f64a4409e5be5
Tanmay Roy <tanmayroy84368@gmail.com>
2024-07-11 18:27:42 +0530

    Add categories on home page

--- src/assets/BikeCategory.js
+++ src/assets/BikeCategory.js
@@ -1,5 +1,16 @@
 export const BikeCategory = [
   {
     name: "Scooter",
+    imagePath: "/images/bike_category/image1.png",
+  },
+
+  {
+    name: "Electric Scooter",
+    imagePath: "/images/bike_category/image2.png",
+  },
+
+  {
+    name: "Bike",
+    imagePath: "/images/bike_category/image3.png",
   },
 ];
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -10,11 +10,32 @@ import { ReviewsData } from "../assets/ReviewsData";
 import { useEffect, useState } from "react";
 import { RiDoubleQuotesL } from "react-icons/ri";
 import { RiDoubleQuotesR } from "react-icons/ri";
+import { BikeCategory } from "../assets/BikeCategory";
 
 const HomePage = () => {
   const heading = `Your<br /><span style="color:${COLORS.yellow}">Urban Adventure</span><br />Starts Here !`;
 
-  const responsive = {
+  const responsive1 = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 1350 },
+      items: 3,
+    },
+    desktop: {
+      breakpoint: { max: 1350, min: 1024 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 730 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 730, min: 0 },
+      items: 1,
+    },
+  };
+
+  const responsive2 = {
     superLargeDesktop: {
       // the naming can be any, depends on you.
       breakpoint: { max: 4000, min: 1350 },
@@ -34,11 +55,11 @@ const HomePage = () => {
     },
   };
 
-  const [showDots, setShowDots] = useState(window.innerWidth <= 550);
+  const [showDots, setShowDots] = useState(window.innerWidth <= 730);
 
   useEffect(() => {
     const handleResize = () => {
-      setShowDots(window.innerWidth <= 550);
+      setShowDots(window.innerWidth <= 730);
     };
 
     window.addEventListener("resize", handleResize);
@@ -59,6 +80,28 @@ const HomePage = () => {
         imagePath="/images/home_banner.png"
       />
 
+      <div className={styles.div1}>
+        <h5 className={styles.div1Heading}>Browse by Style</h5>
+
+        <div className={styles.categoryCarousel}>
+          <Carousel
+            responsive={responsive1}
+            showDots={showDots}
+            arrows={!showDots}
+            // infinite={true}
+          >
+            {BikeCategory.map((item, index) => (
+              <div key={index} className={styles.categoryOuterDiv}>
+                <div className={styles.categoryDiv}>
+                  <img src={item.imagePath} alt="image" />
+                  <p className={styles.categoryName}>{item.name}</p>
+                </div>
+              </div>
+            ))}
+          </Carousel>
+        </div>
+      </div>
+
       <div className={styles.div2}>
         <img
           src="/images/home_page/image1.png"
@@ -74,7 +117,7 @@ const HomePage = () => {
         <div className={styles.carouselContainer}>
           <h5 className={styles.div4Heading}>What Our Clients Say</h5>
           <Carousel
-            responsive={responsive}
+            responsive={responsive2}
             showDots={showDots}
             arrows={!showDots}
             infinite={true}
