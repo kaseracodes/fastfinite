ef9d9e9371fbc3bc2fefcf30fca4d5386b35ba02
Tanmay Roy <tanmayroy84368@gmail.com>
2024-10-25 14:47:35 +0530

    change firebase project

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -306,15 +306,15 @@ const DetailPage = () => {
 
   const handlePayment = async (e) => {
     e.preventDefault();
-    const f = true;
+    // const f = true;
 
-    if (f) {
-      notification["info"]({
-        message: `Booking is disabled till 13th October, 2024`,
-        duration: 3,
-      });
-      return;
-    }
+    // if (f) {
+    //   notification["info"]({
+    //     message: `Booking is disabled till 13th October, 2024`,
+    //     duration: 3,
+    //   });
+    //   return;
+    // }
 
     if (!isChecked) {
       notification["error"]({
